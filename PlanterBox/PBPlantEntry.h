b5f5dd5dad68439b730e8f5471e69f027759b7dc
Jeff Remer <jeff@strava.com>
2012-07-21 16:28:40 -0700

    Ignores build directory

---
+++ PlanterBox/PBPlantEntry.h
@@ -0,0 +1,13 @@
+//
+//  PBPlantEntry.h
+//  PlanterBox
+//
+//  Created by Jeff Remer on 7/21/12.
+//  Copyright (c) 2012 Strava, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PBPlantEntry : NSObject
+
+@end
---
+++ PlanterBox/PBPlantEntry.m
@@ -0,0 +1,13 @@
+//
+//  PBPlantEntry.m
+//  PlanterBox
+//
+//  Created by Jeff Remer on 7/21/12.
+//  Copyright (c) 2012 Strava, Inc. All rights reserved.
+//
+
+#import "PBPlantEntry.h"
+
+@implementation PBPlantEntry
+
+@end
