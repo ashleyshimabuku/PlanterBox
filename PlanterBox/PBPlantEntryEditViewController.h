89d2165684290f544768e5fba7fd0fb2d2fac5d5
Ashley Shimabuku <ashley@threestarchina.com>
2012-07-21 18:06:58 -0700

    Adds start of the plant entry view controller

---
+++ PlanterBox/PBPlantEntryEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  PBPlantEntryEditViewController.h
+//  PlanterBox
+//
+//  Created by Ashley Shimabuku on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PBPlantEntryEditViewController : UIViewController
+
+@end
---
+++ PlanterBox/PBPlantEntryEditViewController.m
@@ -0,0 +1,43 @@
+//
+//  PBPlantEntryEditViewController.m
+//  PlanterBox
+//
+//  Created by Ashley Shimabuku on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PBPlantEntryEditViewController.h"
+
+@interface PBPlantEntryEditViewController ()
+
+@end
+
+@implementation PBPlantEntryEditViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
